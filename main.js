2cdc5e5e368b76b47f1e1290067700d0ddfefa3a
boris <simkinbb@gmail.com>
2021-01-01 20:14:07 +0300

    Сделал комнаты

--- main.js
+++ main.js
@@ -1,18 +1,37 @@
-const server = require('http').createServer();
+const app = require('express')();
+const server = require('http').createServer(app);
+const options = { /* ... */ };
+const io = require('socket.io')(server, options);
+const cors = require('cors')
 
+app.use(cors())
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
-const io = require('socket.io')(server);
+const existingRooms = ['123456789', '123', '11111', '123123']
 
-io.on('connection', (socket) => {
-    console.log(`One user joined the chat with ID: ${ socket.id }`)
-    socket.on('message', data => {
-        /** При получении сообщения рассылаем его всем остальным соединениям
-         * */
-        socket.broadcast.emit('receiveMessage', data)
-    });
+app.get('/api/room/:id/exists', (req, res) => {
+  res.jsonp({
+    exists: existingRooms.includes(req.params.id)
+  })
 
+})
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log({roomId, userId})
+    socket.join(roomId)
+
+    socket.to(roomId).broadcast.emit('userConnected', userId)
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('userDisconnected', userId)
+    })
+
+    socket.on('new-message', data => {
+      socket.to(roomId).broadcast.emit('newMessage', data)
+    })
+  })
 })
 
 
