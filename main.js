1c65b6cdb40b774eb0aab7754e1516c3757cd567
boris <simkinbb@gmail.com>
2021-01-17 16:48:55 +0300

    Добавил раздачу статики

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
+const express = require('express')
 const app = require('express')();
 const server = require('http').createServer(app);
-const options = { /* ... */ };
-const io = require('socket.io')(server, options);
+const io = require('socket.io')(server);
 const cors = require('cors')
-// const { PeerServer } = require('peer')
+const { PeerServer } = require('peer')
 
 app.use(cors())
 
-// const peerServer = PeerServer({port: 9000, path: '/myapp'})
+const peerServer = PeerServer({port: 3001, path: '/'})
 
 const port = process.env.PORT || 3000
 
 const existingRooms = ['123456789', '123', '11111', '123123']
+
+app.use(express.static(__dirname + '/dist'));
+
 
 app.get('/api/room/:id/exists', (req, res) => {
   res.jsonp({
@@ -20,8 +23,29 @@ app.get('/api/room/:id/exists', (req, res) => {
 
 })
 
+app.get('/api/room/:id/users', (req, res) => {
+  let sockets = io.sockets.adapter.rooms[req.params.id].sockets
+  for (let socketId of Object.keys(sockets)) {
+    let clientSocket = io.sockets.connected[socketId];
+    console.log(clientSocket.user.userId)
+  }
+  res.jsonp({
+    exists: existingRooms.includes(req.params.id)
+  })
+
+})
+
+app.use('/*', express.static(__dirname + '/dist'));
+
+
+
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
+    console.log(socket.test)
+    socket.user = {
+      userId,
+
+    }
     console.log({roomId, userId})
     socket.join(roomId)
 
@@ -32,6 +56,7 @@ io.on('connection', socket => {
     })
 
     socket.on('new-message', data => {
+      console.log(socket.user)
       socket.to(roomId).broadcast.emit('newMessage', data)
     })
 
