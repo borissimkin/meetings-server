25f3df4b7c934030656f61ffdb938f785ef5c41e
boris <simkinbb@gmail.com>
2021-02-07 14:31:37 +0300

    Добавил авторизацию

---
+++ app/middlewares/attachCurrentUser.js
@@ -0,0 +1,12 @@
+export default (req, res, next) => {
+ const decodedTokenData = req.tokenData;
+ const userRecord = await UserModel.findOne({ _id: decodedTokenData._id })
+
+  req.currentUser = userRecord;
+
+ if(!userRecord) {
+   return res.status(401).end('User not found')
+ } else {
+   return next();
+ 
+}
